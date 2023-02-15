cb9e74ff3ebb7fe2318352a5e85a68b4fc81c666
Victor <vctudo.27@gmail.com>
2023-02-14 21:28:33 -0300

    🎨

---
+++ lib/app.dart
@@ -0,0 +1,9 @@
+import 'package:mac_scanner/view/home.dart';
+import 'package:flutter/material.dart';
+
+class App extends MaterialApp {
+  const App({super.key});
+
+  @override
+  Widget get home => const Home();
+}
