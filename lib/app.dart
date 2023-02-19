85cd198633681784087c7ce086d123fe6a1651af
Victor <vctudo.27@gmail.com>
2023-02-18 23:18:39 -0300

    💄created color system

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,27 @@
+import 'package:flutter/services.dart';
 import 'package:mac_scanner/view/home.dart';
 import 'package:flutter/material.dart';
+import 'package:mac_scanner/view/style/colors.dart';
 
 class App extends MaterialApp {
   const App({super.key});
 
   @override
   Widget get home => const Home();
+
+  @override
+  ThemeData get theme => ThemeData(
+        colorScheme: const ColorScheme(
+            brightness: Brightness.light,
+            background: AppColors.backgroundColor,
+            onBackground: Colors.white,
+            primary: AppColors.primary,
+            onPrimary: AppColors.onPrimary,
+            secondary: AppColors.secondary,
+            onSecondary: AppColors.onSecondary,
+            surface: AppColors.onPrimary,
+            onSurface: Colors.black,
+            error: Colors.red,
+            onError: Colors.redAccent),
+      );
 }
