07f4d12b107e5932d56c731fbba0a3ae6c2265e8
Victor <vctudo.27@gmail.com>
2023-02-18 23:20:10 -0300

    💄 created color system

---
+++ lib/view/style/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const backgroundColor = Color(0xFF252422);
+  static const primary = Color(0xFFEB5E28);
+  static const onPrimary = Color(0xFFCCC5B9);
+  static const onSecondary = Color(0xFF4B4843);
+  static const secondary = Color(0xFF403D39);
+}
