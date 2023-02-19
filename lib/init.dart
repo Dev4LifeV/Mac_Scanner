1321bff0edc230572e0c914681856261d2069bff
Victor <vctudo.27@gmail.com>
2023-02-18 23:19:37 -0300

    💄changed statusbar color

--- lib/init.dart
+++ lib/init.dart
@@ -1,9 +1,17 @@
+import 'package:flutter/services.dart';
 import 'package:mac_scanner/controller/controller_get_mac_address.dart';
 import 'package:mac_scanner/core/config/injector.dart';
 import 'package:mac_scanner/core/config/method_channels/http_channel.dart';
 import 'package:mac_scanner/usecase/usecase_get_mac_address.dart';
+import 'package:flutter/material.dart';
 
 init() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle.dark.copyWith(
+      statusBarBrightness: Brightness.dark,
+      statusBarColor: Colors.white,
+    ),
+  );
   _createChannels();
   _createUsecases();
   _createControllers();
